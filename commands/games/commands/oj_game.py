710ce5796e55a3ab3cadd5fc6d9fc55f0399bbce
AlguienSama <marcfp2001@gmail.com>
2022-07-30 22:41:21 +0200

    oj game fight

--- commands/games/commands/oj_game.py
+++ commands/games/commands/oj_game.py
@@ -22,19 +22,19 @@ class DefendAction(discord.ui.View):
         super().__init__(timeout=20.0)
         self.Game = game
     
-    @discord.ui.button(label='Defender', emoji='\U00002705', style=discord.ButtonStyle.green, custom_id='defend_action:green')
+    @discord.ui.button(label='Defender', emoji='\U0001F6E1', style=discord.ButtonStyle.green, custom_id='defend_action:green')
     async def green(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.edit_message(content=f'Defensor: {self.Game.attack.user.mention}')
         self.stop()
         await self.Game.action('defend')
 
-    @discord.ui.button(label='Esquivar', emoji='\U000027A1', style=discord.ButtonStyle.blurple, custom_id='defend_action:blue')
+    @discord.ui.button(label='Esquivar', emoji='\U0001F4A8', style=discord.ButtonStyle.blurple, custom_id='defend_action:blue')
     async def blue(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.edit_message(content=f'Defensor: {self.Game.attack.user.mention}')
         self.stop()
         await self.Game.action('dodge')
 
-    @discord.ui.button(label='Rendirse', emoji='\U0000274C', style=discord.ButtonStyle.red, custom_id='defend_action:red')
+    @discord.ui.button(label='Rendirse', emoji='\U00002716', style=discord.ButtonStyle.red, custom_id='defend_action:red')
     async def red(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.edit_message(content=f'Defensor: {self.Game.attack.user.mention}')
         self.stop()
@@ -78,28 +78,26 @@ class Game:
             await self.msg.edit(embed=self.embed(), view=DefendAction(self))
     
     async def action(self, action):
-        print('Action:', action)
-        # defense or dodge
         if action == 'defend':
             total_defense = self.deffend.defense + self.rand_dice()
             damage = self.total_damage - total_defense
-            self.last_turn_action = f'**Defendido** con **{self.deffend.defense}** + 🎲 {total_defense - self.deffend.defense} = **{total_defense}**'
+            self.last_turn_action = f'**Defendido** con {self.deffend.defense} + 🎲 {total_defense - self.deffend.defense} = **{total_defense}**'
             realized_damage = damage if damage > 0 else 1
             self.last_turn_action += f'\nDaño realizado: {realized_damage}'
             self.deffend.life -= realized_damage
         elif action == 'dodge':
             total_dodge = self.deffend.dodge + self.rand_dice()
             dodged = self.total_damage < total_dodge
-            self.last_turn_action = f'**Esquivado** con **{self.deffend.dodge}** + 🎲 {total_dodge - self.deffend.dodge} = **{"ÉXITO" if dodged else "FALLIDO"}**'
-            self.deffend.life -= self.total_damage if dodged else 0
+            self.last_turn_action = f'**Esquivado** con {self.deffend.dodge} + 🎲 {total_dodge - self.deffend.dodge} = **{"ÉXITO" if dodged else "FALLIDO"}**'
+            self.deffend.life -= 0 if dodged else self.total_damage
         else:
             self.last_turn_action = F'**Rendido**'
+            self.last_turn_action = f'**Rendido**'
             self.deffend.life = 0
+        
         if self.winner():
-            print('Winner')
-            return self.winner()
+            await self.end_game()
         else:
-            print('Next turn')
             await self.start()
         pass
     
@@ -133,16 +131,19 @@ class Game:
             return self.player1
         else:
             return False
+    
+    def embed_user_data(self, user):
+        return f'**💖 {user.life}**\n🗡 {user.damage}\n🛡 {user.defense}\n💨 {user.dodge}'
+    
+    async def end_game(self):
+        embed = Embed(title='OJ Game', description=f'{self.user1.mention} vs {self.user2.mention}\n**GANADOR: {self.attack.user.mention}** # Turno **{self.turn}**').add_field(title=f'{self.user1}', desc=self.embed_user_data(self.player1), inline=True).add_field(title=f'{self.user2}', desc=self.embed_user_data(self.player2), inline=True).add_field(title='Movimiento anterior', desc=f'Daño de **{self.deffend.user}**: {self.deffend.damage} + 🎲 {self.last_turn_damage-self.deffend.damage} = **{self.last_turn_damage}**\nAcción de **{self.attack.user}**: {self.last_turn_action}').success()
+        await self.msg.edit(content=f'**{self.attack.user.mention}** gana la partida!', embed=embed.get_embed())
         
     def embed(self):
-        def description(user):
-            return f'**💖 {user.life}**\n🗡 {user.damage}\n🛡 {user.defense}\n🌪 {user.dodge}'
-        
-        attack, defense = self.get_users()
-        embed = Embed(title='OJ Game', description=f'Turno: {self.turn}\nAtacante: {attack.mention}\nDefensor: {defense.mention}').add_field(title=f'{self.user1}', desc=description(self.player1), inline=True).add_field(title=f'{self.user2}', desc=description(self.player2), inline=True)
+        embed = Embed(title='OJ Game', description=f'Turno: {self.turn}\nAtacante: {self.attack.user.mention}\nDefensor: {self.deffend.user.mention}').add_field(title=f'{self.user1}', desc=self.embed_user_data(self.player1), inline=True).add_field(title=f'{self.user2}', desc=self.embed_user_data(self.player2), inline=True)
         if self.last_turn_action is not None:
-            embed.add_field(title='Movimiento anterior', desc=f'Daño de **{defense}**: {self.deffend.damage} + 🎲 {self.last_turn_damage-self.deffend.damage} = **{self.last_turn_damage}**\nAcción de **{attack}**: {self.last_turn_action}')
-        embed.add_field(title='Movimiento', desc=f'Daño de **{attack}**: {self.attack.damage} + 🎲 {self.total_damage-self.attack.damage} = **{self.total_damage}**\nQue acción desea realizar **{defense}**?')
+            embed.add_field(title='Movimiento anterior', desc=f'Daño de **{self.deffend.user}**: {self.deffend.damage} + 🎲 {self.last_turn_damage-self.deffend.damage} = **{self.last_turn_damage}**\nAcción de **{self.attack.user}**: {self.last_turn_action}')
+        embed.add_field(title='Movimiento', desc=f'Daño de **{self.attack.user}**: {self.attack.damage} + 🎲 {self.total_damage-self.attack.damage} = **{self.total_damage}**\nQue acción desea realizar **{self.deffend.user}**?')
         return embed.get_embed()
 
 
