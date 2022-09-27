35e600c4e09aecf6ff3fa44ac13eb3de6a922dec
AlguienSama <marcfp2001@gmail.com>
2022-09-27 02:31:08 +0200

    give money

---
+++ commands/economy/commands/give.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext.commands import Context, UserConverter, Us
+from utils.errors import CustomError, MoneyError
+from utils.responses.Embed import Embed
+from utils.ddbb.economy import *
+from utils.logs.economy import win_money, lose_money
+
+
+async def balance(ctx: Context, user: discord.User, money: int):
+    try:
+        user = await UserConverter().convert(ctx, user)
+    except:
+        raise CustomError('Usuario inválido')
+
+    try:
+        money = int(money)
+        if money < 1:
+            raise MoneyError(min=1)
+    except:
+        raise MoneyError(min=1)
+    await check_bal(ctx.guild.id, ctx.author.id, money)
+    
+    await lose_money(ctx, ctx.author, money, f'Give to {user.id}')
+    await win_money(ctx, user, money, f'Recive from {ctx.author.id}')
+    
+    embed = Embed(title='Donación', user=ctx.author, description=f'<@{ctx.author.id}> ha donado {money}💰 a <@{user.id}>').success()
+    
+    await ctx.channel.send(embed=embed)    
