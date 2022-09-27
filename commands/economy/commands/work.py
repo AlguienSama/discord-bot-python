1abd20e1a46bd5a7c35e61d1c5abfb6bccb8d00c
AlguienSama <marcfp2001@gmail.com>
2022-09-27 18:06:05 +0200

    work logs

--- commands/economy/commands/work.py
+++ commands/economy/commands/work.py
@@ -1,15 +1,14 @@
 import random
 from discord.ext.commands import Context
-from utils.ddbb.economy import update_work
+from utils.logs.economy import win_money
 from utils.responses.Embed import Embed
 
 
 async def work(ctx: Context):
 
     money = random.randint(1000, 2000)
-    await update_work(ctx.guild.id, ctx.author.id, money)
+    await win_money(ctx, ctx.author, money, 'work')
 
-    embed = Embed(title='Work', description=f'Has obtenido **{money:,}** haikoins', user=ctx.author) \
-        .economy().get_embed()
+    embed = Embed(title='Work', description=f'Has obtenido **{money:,}** haikoins', user=ctx.author).economy().get_embed()
 
     return await ctx.send(embed=embed)
